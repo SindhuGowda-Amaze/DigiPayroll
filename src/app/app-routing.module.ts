c1d3fb2b93e345043e1ac4863090b6c376d4feba
Rupali Pawar <naveen.amazeinc@gmail.com>
2021-12-11 17:11:35 +0530

    Naveen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyAdjustmentFormComponent } from './company-adjustment-form/company-adjustment-form.component';
 import { CompanyProfileComponent } from './Pages/company-profile/company-profile.component';
+import { PayRollFormComponent } from './pay-roll-form/pay-roll-form.component';
+import { PhilHealthFormComponent } from './phil-health-form/phil-health-form.component';
+import { SssFormComponent } from './sss-form/sss-form.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 const routes: Routes = [
   {
     path: "CompanyProfile", component: CompanyProfileComponent
+  },
+  {
+    path: "sssForm", component: SssFormComponent
+  },
+  {
+    path: "PayRollForm", component: PayRollFormComponent
+  },
+  {
+    path: "UserForm", component: UserFormComponent
+  },
+  {
+    path: "PhilHealthForm", component: PhilHealthFormComponent
+  },
+  {
+    path: "CompanyAdjustmentForm", component: CompanyAdjustmentFormComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CompanyProfileComponent } from './Pages/company-profile/company-profile.component';
 import { HeaderComponent } from './Pages/header/header.component';
+import { SssFormComponent } from './sss-form/sss-form.component';
+import { PayRollFormComponent } from './pay-roll-form/pay-roll-form.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { PhilHealthFormComponent } from './phil-health-form/phil-health-form.component';
+import { CompanyAdjustmentFormComponent } from './company-adjustment-form/company-adjustment-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyProfileComponent,
-    HeaderComponent
+    HeaderComponent,
+    SssFormComponent,
+    PayRollFormComponent,
+    UserFormComponent,
+    PhilHealthFormComponent,
+    CompanyAdjustmentFormComponent
   ],
   imports: [
     BrowserModule,
