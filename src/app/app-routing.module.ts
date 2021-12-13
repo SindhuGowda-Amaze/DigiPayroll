f0c81b753d771b5d1d1e4922b30988ef5b2e2ccf
Anusha <anushavalmiki.amaze@gmail.com>
2021-12-13 16:04:55 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,7 @@ import { CompanyProfileComponent } from './Pages/company-profile/company-profile
 import { DepartmentModifyComponent } from './Pages/department-modify/department-modify.component';
 import { DepartmentComponent } from './Pages/department/department.component';
 import { PayrollComponent } from './Pages/payroll/payroll.component';
-import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { EmployeeFormComponent } from './Pages/employee-form/employee-form.component';
 
 const routes: Routes = [
   {
