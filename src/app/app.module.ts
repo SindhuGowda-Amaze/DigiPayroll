bb07475b27c7e923493adeb7f639271234a7a90d
Anusha <anushavalmiki.amaze@gmail.com>
2021-12-17 11:01:38 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DepartmentComponent } from './Pages/department/department.component';
@@ -74,9 +75,11 @@ import { ApplyLeaveComponent } from './Pages/apply-leave/apply-leave.component';
     AppRoutingModule,
     FormsModule,
     NgbModule,
-    NgxDropzoneModule
+    NgxDropzoneModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
