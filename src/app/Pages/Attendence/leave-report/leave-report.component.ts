446e3fed925cc5c90aa663da1a9cd1d026839fd5
Rupali Pawar <naveen.amazeinc@gmail.com>
2021-12-22 18:11:31 +0530

    nn

--- src/app/Pages/Attendence/leave-report/leave-report.component.ts
+++ src/app/Pages/Attendence/leave-report/leave-report.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DigipayrollServiceService } from 'src/app/digipayroll-service.service';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-leave-report',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaveReportComponent implements OnInit {
 
-  constructor() { }
+  constructor(private DigipayrollServiceService: DigipayrollServiceService, private ActivatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    debugger
+    this.GetMyLeaveReport();
+  }
+
+  myleaveReportList:any;
+
+  public GetMyLeaveReport() {
+    debugger
+    this.DigipayrollServiceService.GetMyLeaveReport().subscribe(data=>{
+      debugger
+      this.myleaveReportList=data ;
+      debugger
+     })
   }
 
   refresh(){
