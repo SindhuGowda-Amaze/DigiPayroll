a7b535ea8f16b357e5a4bd594099f049a459fc7e
Rupali Pawar <naveen.amazeinc@gmail.com>
2021-12-31 17:59:31 +0530

    NNNN

---
+++ src/app/currency-master/currency-master.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CurrencyMasterComponent } from './currency-master.component';
+
+describe('CurrencyMasterComponent', () => {
+  let component: CurrencyMasterComponent;
+  let fixture: ComponentFixture<CurrencyMasterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CurrencyMasterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CurrencyMasterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
