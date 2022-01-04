617035e0050b6186a397fc0235de796199c38b3a
PraveenKumarVelu28 <praveenkumaramze@gmail.com>
2022-01-04 14:38:01 +0530

    commit all

--- src/app/sssr3-report/sssr3-report.component.ts
+++ src/app/sssr3-report/sssr3-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as XLSX from 'xlsx';
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
 @Component({
   selector: 'app-sssr3-report',
   templateUrl: './sssr3-report.component.html',
@@ -10,9 +12,30 @@ export class SSSR3ReportComponent implements OnInit {
   constructor() { }
   Month:any;
   Year:any;
+  loader:any;
   ngOnInit(): void {
     this.Month="jan";
     this.Year="2021";
+    this.loader = false;
   }
+
+
+  fileName = 'R3 Report.xlsx';
+  exportexcel(): void {
+    this.loader = true;
+    /* table id is passed over here */
+    let element = document.getElementById('download');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+    this.loader = false;
+  }
+
+
 
 }
