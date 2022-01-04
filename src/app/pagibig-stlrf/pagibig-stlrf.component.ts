a07a2e522ae0a0032094441927bbac445bd50b9c
PraveenKumarVelu28 <praveenkumaramze@gmail.com>
2022-01-04 11:54:55 +0530

    commit all

--- src/app/pagibig-stlrf/pagibig-stlrf.component.ts
+++ src/app/pagibig-stlrf/pagibig-stlrf.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-pagibig-stlrf',
@@ -13,10 +16,67 @@ export class PagibigSTLRFComponent implements OnInit {
   Year:any;
   Loan:any;
   Person:any;
+  showleaseforprint:any;
+  loader:any;
   ngOnInit(): void {
     this.Month="jan";
     this.Year="2021";
     this.Loan="HDMF"
-    this.Person="Admin"
+    this.Person="Admin";
+    this.showleaseforprint = 0;
+    this.loader=false;
+  }
+
+
+
+
+
+  public showpdf(){
+    this.showleaseforprint = 1;
+  }
+
+
+  public convetToPDF1() {
+    debugger
+   this.loader=true
+    var data: any = document.getElementById('downloadaplication');
+    html2canvas(data).then(canvas => {
+   
+      var margin = 5;
+      var imgWidth = 208
+      // var pageHeight = 295 - 10 * margin;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      var doc = new jsPDF('p', 'mm');
+      var position = 0;
+      while (heightLeft > 0) {
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        position = heightLeft - imgHeight;
+
+        doc.addPage();
+
+
+        doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      
+        heightLeft -= pageHeight;
+
+      }
+      doc.deletePage(1)
+      doc.save('STLRF.pdf');
+      this.loader=false
+      var pdf1 = doc.output('blob');
+      var file = new File([pdf1], "Application.pdf");
+      let body = new FormData();
+      debugger
+      body.append('Dan', file);
+      console.log('pdf', pdf1)
+    
+
+
+    }).then(() => {
+     
+    });;
   }
 }
--- src/app/sssrl1-report/sssrl1-report.component.ts
+++ src/app/sssrl1-report/sssrl1-report.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
+
 @Component({
   selector: 'app-sssrl1-report',
   templateUrl: './sssrl1-report.component.html',
@@ -12,10 +15,61 @@ export class SSSRL1ReportComponent implements OnInit {
   Month:any;
   Year:any;
   Person:any;
+  showleaseforprint:any;
   ngOnInit(): void {
     this.Month="jan";
     this.Year="2021";
-    this.Person="Admin"
+    this.Person="Admin";
+    this.showleaseforprint = 0;
+  }
+
+  public showpdf(){
+    this.showleaseforprint = 1;
+  }
+
+
+  public convetToPDF1() {
+    debugger
+   
+    var data: any = document.getElementById('downloadaplication');
+    html2canvas(data).then(canvas => {
+   
+      var margin = 5;
+      var imgWidth = 208
+      // var pageHeight = 295 - 10 * margin;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      var doc = new jsPDF('p', 'mm');
+      var position = 0;
+      while (heightLeft > 0) {
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        position = heightLeft - imgHeight;
+
+        doc.addPage();
+
+
+        doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      
+        heightLeft -= pageHeight;
+
+      }
+      doc.deletePage(1)
+      doc.save('PayrollSummary.pdf');
+      
+      var pdf1 = doc.output('blob');
+      var file = new File([pdf1], "Application.pdf");
+      let body = new FormData();
+      debugger
+      body.append('Dan', file);
+      console.log('pdf', pdf1)
+    
+
+
+    }).then(() => {
+     
+    });;
   }
 
 
