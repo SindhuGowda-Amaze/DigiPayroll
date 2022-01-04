8cbde6171fca211d1f10a3ea39ac2715a6ab08ef
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-01-04 15:59:53 +0530

    dsd

--- src/app/adjustment-generate-report/adjustment-generate-report.component.ts
+++ src/app/adjustment-generate-report/adjustment-generate-report.component.ts
@@ -12,14 +12,29 @@ export class AdjustmentGenerateReportComponent implements OnInit {
 
   showtable:any;
   constructor() { }
-  check:any;
+  selecallbtn:any;
   ngOnInit(): void {
-    this.check=false;
+    this.selecallbtn = false;
+    this.selectone = false;
   }
 
-  selectall(){
-   this.check=true;
+  selectone:any;
+  selectALL(checked: boolean) { // pass true or false to check or uncheck all
+    this.selecallbtn = true;
+    this.selectone = false;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type == "checkbox") {
+        inputs[i].checked = checked;
+        // This way it won't flip flop them and will set them all to the same value which is passed into the function
+      }
+    }
   }
+
+  showone(){
+    this.selectone = true;;
+  }
+
 
   
   fileName = 'Approved Applicants Reports.xlsx';
