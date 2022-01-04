2bbfd750b69dd44300213b3e25a09c3e74c8a7a2
PraveenKumarVelu28 <praveenkumaramze@gmail.com>
2022-01-04 10:08:28 +0530

    commit all praveen

--- src/app/certificate-of-contribution/certificate-of-contribution.component.ts
+++ src/app/certificate-of-contribution/certificate-of-contribution.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certificate-of-contribution.component.css']
 })
 export class CertificateOfContributionComponent implements OnInit {
-
+  showleaseforprint:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.showleaseforprint = 0;
   }
+
+
+  public showpdf(){
+    this.showleaseforprint = 1;
+  }
+
 
 }
